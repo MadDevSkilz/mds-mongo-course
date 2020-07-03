fee486d4835106eedeb997e0303d5a00c1d83800
Bruce Van Horn <bvanhorn@cleartechnologies.net>
2020-07-03 13:54:18 -0500

    added sub-model schema and validations

--- node-mongoose/models/planet.js
+++ node-mongoose/models/planet.js
@@ -2,6 +2,26 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const discoveredBySchema = new Schema({
+    firstName: {
+        type: String,
+        trim: true,
+        required: "The first name must be specified.  Use 'Unknown' if the name is unknown."
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: "The last name must be specified.  Use 'Unknown' if the name is unknown."
+    },
+    year: {
+        type: Number
+    },
+    origin: {
+        type: String,
+        trim: true
+    }
+});
+
 const planetSchema = new Schema({
     planetName: {
         type: String,
@@ -14,16 +34,19 @@ const planetSchema = new Schema({
 
     },
     mass: {
-        type: Number
+        type: Number,
+        min: 600e18
     },
     radius: {
-        type: Number
+        type: Number,
+        min: 400
     },
     distanceFromEarth: {
-        type: Number
+        type: Number,
+        min: 0
     },
     nearbyStars: [String],
-    discoveredBy: {}
+    discoveredBy: discoveredBySchema
 });
 
 const Planet = mongoose.model("Planet", planetSchema);
